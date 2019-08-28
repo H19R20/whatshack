3befb28d52f6b575c5ece983302c85e6e40e292c
H19R20 <54589631+H19R20@users.noreply.github.com>
2019-08-28 17:27:01 +0700

    Update whatshack.sh

--- whatshack.sh
+++ whatshack.sh
@@ -9,8 +9,8 @@ echo "
 
 Copyright (c) 2018 ceweberbatang Ganteng";
 echo "";
-read -p "Nomer Kamu (+62xxxx)=> " nomer;
-read -p "Nomer Target (+62xxxx) => " target;
+read -p "Nomer Kamu (+62xxxx)=> "+6282335713339;
+read -p "Nomer Target (+62xxxx) => "+628997461151;
 sleep 2
 echo "[+] Syncing Target...";
 sleep 1
